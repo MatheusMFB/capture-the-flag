7cb388e43d6eece728aece03d14576e45acbb854
Matheus Ferreira <musicmfb38@gmail.com>
2020-07-27 14:45:48 -0300

    capture-the-flag
    site de CTF, site com sistema de login e cadastro de equipes, painel de admin, site com funções ainda a ser desenvilvidas.

---
+++ iniciar.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>CTF WYDEN 2020</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css" />
+        <link  rel="stylesheet" href="css/font.css">
+        <link rel="shortcut icon" type="image/png" href="image/logo.png" />
+        <style type="text/css">
+            body {
+                width: 100%;
+                background-image: url(image/imagem.jpg) ;
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: cover;
+            }
+            #capture{height: 350px; width: 400px; margin-top: 150px; margin-left: 900px;}
+            .posicao{position: relative;}
+            #botao{margin-left: 450px; margin-top: 360px;}
+        </style>
+    </head>
+    <body>
+        <center>
+                <div class="posicao"> 
+                    <img src="image/capture.png" id="capture">
+                </div>
+            <div class="intro" id="botao">
+                <a href="login.php" class="btn"> login </a> &emsp;
+                <a href="register.php" class="btn"> CADASTRE-SE </a>
+            </div>
+        </center>
+    </body>
+</html>
